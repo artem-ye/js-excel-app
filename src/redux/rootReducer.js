a7f662d578427bb5fe32090f9b79238d3f86b019
ay <shlemafona@gmail.com>
2021-06-22 16:01:16 +0300

    Unnessasary coments cleaned

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,5 +1,10 @@
-// import { toInlineStyles } from '../core/utils';
-import { CHANGE_TEXT, CHANGE_STYLES, TABLE_RESIZE, APPLY_STYLE, CHANGE_TITLE } from './types';
+import {
+    CHANGE_TEXT,
+    CHANGE_STYLES,
+    TABLE_RESIZE,
+    APPLY_STYLE,
+    CHANGE_TITLE
+} from './types';
 
 export function rootReducer(state, action) {
     let dataField;
@@ -20,11 +25,13 @@ export function rootReducer(state, action) {
                 currentText: action.data.value,
                 [dataField]: value(state, dataField, action)
             };
+
         case CHANGE_STYLES:
             return {
                 ...state,
                 currentStyles: action.data
             };
+
         case APPLY_STYLE:
             dataField = 'stylesState';
             val = state[dataField] || {};
@@ -38,14 +45,9 @@ export function rootReducer(state, action) {
                 [dataField]: val,
                 currentStyles: {...state.currentStyles, ...action.data.value}
             };
+
         case CHANGE_TITLE:
             dataField = 'titleText';
-            // console.log('Title text');
-            // console.log({
-            //     ...state,
-            //     [dataField]: action.data
-            // });
-
             return {
                 ...state,
                 [dataField]: action.data
@@ -61,4 +63,3 @@ function value(state, field, action) {
     val[action.data.id] = action.data.value;
     return val;
 }
-
